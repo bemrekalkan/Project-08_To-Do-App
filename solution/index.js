f76c6221e67893c2c20f00361529f55b3410bd2a
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-11 15:27:33 +0300

    going

--- solution/index.js
+++ solution/index.js
@@ -1,5 +1,21 @@
-const todo = document.getElementById("todo-button");
+const btn = document.getElementById("todo-button");
 
 const todoInput = document.getElementById("todo-input");
 
 const todoUl = document.getElementById("todo-ul");
+
+window.onload = function () {
+  todoInput.focus();
+};
+
+btn.addEventListener("click", (e) => {
+  todoUl.innerHTML += `<li><i class="fa fa-check"></i><p>${todoInput.value}</p><i class="fa fa-trash"></i></li>`;
+
+  todoInput.value = "";
+});
+
+todoInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    btn.click();
+  }
+});
