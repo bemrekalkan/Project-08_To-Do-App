c1ea2a8ccfda78ee8adeca97217cc74d86d7d122
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-11 15:51:36 +0300

    ...

--- solution/index.js
+++ solution/index.js
@@ -1,30 +1,39 @@
+//? selectors:
 const btn = document.getElementById("todo-button");
 
 const todoInput = document.getElementById("todo-input");
 
 const todoUl = document.getElementById("todo-ul");
 
+//? baslangicta input aktif olsun:
 window.onload = function () {
   todoInput.focus();
 };
 
+//? add buton event inin tanimlanmasi:
 btn.addEventListener("click", (e) => {
-  todoUl.innerHTML += `
-  <li>
-    <i class="fa fa-check"></i>
-    <p>${todoInput.value}</p>
-    <i class="fa fa-trash"></i>
-  </li>`;
+  if (!todoInput.value) {
+    alert(`Please enter your todo`);
+  } else {
+    todoUl.innerHTML += `
+        <li>
+          <i class="fa fa-check"></i>
+          <p>${todoInput.value}</p>
+          <i class="fa fa-trash"></i>
+        </li>`;
 
-  todoInput.value = "";
+    todoInput.value = "";
+  }
 });
 
+//? Klavyeden enter tusuna basilmasi ile add butonunun click fonk. cagrilmasi:
 todoInput.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     btn.click();
   }
 });
 
+//? delete ve ok butonlari icin event tanimlamasi:
 todoUl.addEventListener("click", (e) => {
   //! Event sil butonlarinin birisinden geldiyse;
   if (e.target.classList.contains("fa-trash")) {
@@ -32,6 +41,7 @@ todoUl.addEventListener("click", (e) => {
     e.target.parentElement.remove();
   }
 
+  //! Event silme ve ok butonlarinin birisinden geldiyse;
   if (e.target.classList.contains("fa-check")) {
     //? ilgili li elementinde checked adinda bir class varsa bunu sil:
     if (e.target.parentElement.classList.contains("checked")) {
